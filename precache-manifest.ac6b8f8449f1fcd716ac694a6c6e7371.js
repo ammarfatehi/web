f7f3d6a0ee617a3f2ec8dd16bdb5615578fa14eb
ammarfatehi <ammarfatehi@gmail.com>
2020-08-05 04:13:43 -0500

    Updates

---
+++ precache-manifest.ac6b8f8449f1fcd716ac694a6c6e7371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e833a88fde4663c77b4a24a8f84c346",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "864532c4dffd226cce10",
+    "url": "/web/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f044e1151627a136f5bd",
+    "url": "/web/static/css/main.5e53184a.chunk.css"
+  },
+  {
+    "revision": "864532c4dffd226cce10",
+    "url": "/web/static/js/2.33a820ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web/static/js/2.33a820ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f044e1151627a136f5bd",
+    "url": "/web/static/js/main.1b7eef8e.chunk.js"
+  },
+  {
+    "revision": "33576ee135edfa2096e9",
+    "url": "/web/static/js/runtime-main.636561be.js"
+  },
+  {
+    "revision": "5d1ba06fe97b61a31cff8c5e20836690",
+    "url": "/web/static/media/resume.5d1ba06f.PNG"
+  }
+]);
